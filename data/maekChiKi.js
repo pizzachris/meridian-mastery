985285f2af7ec9bb6f3810f9f0aee1bd52a6087a
pizzachris <pizzachris2@gmail.com>
2025-05-19 12:43:51 -0400

    Create maekChiKi.js

---
+++ data/maekChiKi.js
@@ -0,0 +1,11 @@
+export const maekChiKi = [
+  {
+    korean: "율결",
+    romanized: "Yuhl Gyuhl",
+    meridian: "LU7",
+    english: "Broken Sequence",
+    healingUse: "Opens chest, clears throat, regulates water",
+    martialUse: "Disrupts breath, shocks Lung meridian, stuns limb"
+  },
+  // ... other 14 points
+];
