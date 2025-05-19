f0b610269491f21a993a9c3e9a815bb2d952a3d5
pizzachris <pizzachris2@gmail.com>
2025-05-19 14:23:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,27 @@ import { maekChiKi } from './data/maekChiKi.js';
 
 let index = 0;
 
+const card = document.getElementById('card');
+const cardInner = document.getElementById('cardInner');
 const front = document.getElementById('front');
 const back = document.getElementById('back');
 const flipBtn = document.getElementById('flipBtn');
 const nextBtn = document.getElementById('nextBtn');
-const card = document.getElementById('card');
 
 function updateCard() {
   const point = maekChiKi[index];
 
   front.innerHTML = `
-    <div>
-      <span style="font-size: 24px; font-weight: bold;">${point.romanized}</span>
-      <br/>
-      <span style="font-size: 16px; color: #666;">${point.meridian}</span>
-      <br/>
-      <em style="font-size: 18px; margin-top: 10px;">${point.english}</em>
-      <br/>
-      <span style="font-size: 28px; color: #c00; margin-top: 10px;">${point.korean}</span>
-    </div>
+    <strong>${point.romanized}</strong><br/>
+    ${point.meridian}<br/>
+    <em>${point.english}</em>
+    <div class="point-korean">${point.korean}</div>
   `;
 
   back.innerHTML = `
-    <div>
-      <strong>Meaning:</strong><br/> ${point.english}<br/><br/>
-      <strong>Healing Use:</strong><br/> ${point.healingUse}<br/><br/>
-      <strong>Martial Use:</strong><br/> ${point.martialUse}
-    </div>
+    <strong>Meaning:</strong> ${point.english}<br/>
+    <strong>Healing Use:</strong> ${point.healingUse}<br/>
+    <strong>Martial Use:</strong> ${point.martialUse}
   `;
 }
 
